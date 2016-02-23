b44a4aa38999d76275be789f8d8be795ee71b773
Wellington T <wtds.trabalho@gmail.com>
2016-02-23 16:04:20 -0300

    Movido para controller

--- app/controllers/inicio_controller.rb
+++ app/controllers/inicio_controller.rb
@@ -1,2 +1,13 @@
 class InicioController < ApplicationController
+
+def index
+	url = "http://quemcuida.com.br/blog/feed/"
+	_feed = Feedjira::Feed.fetch_and_parse url
+
+	@links=[]
+	_feed.entries.take(5).each do |post|
+		@links << [ [ post.title, post.url ], post.published ]
+	end
+end
+
 end
